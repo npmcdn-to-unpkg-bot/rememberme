b3930777be4d4a48bdecf90bff13c4d595cf291d
Kevin Storberg <kevin@storberg.net>
2016-04-13 15:59:15 -0400

    created session models/routes and added a navigation bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'user#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   resources :users
   get 'welcome/index'
 
