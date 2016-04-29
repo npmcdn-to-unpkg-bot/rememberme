0f08f29c7e7e030e14c200a0c7f10e137cdefa6e
Kevin Storberg <kevin@storberg.net>
2016-04-29 19:31:39 -0400

    created route for new deceaseds

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
+  resources :deceaseds
+  get '/memorial/new', to: 'deceaseds#new' 
+
 
 
   resources :users
