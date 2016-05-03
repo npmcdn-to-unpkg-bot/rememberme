931b724231c383c314f54a28f9129e4bde6a70f0
Kevin Storberg <kevin@storberg.net>
2016-05-02 23:19:49 -0400

    set up memorial controller

--- app/controllers/deceaseds_controller.rb
+++ app/controllers/deceaseds_controller.rb
@@ -1,4 +1,16 @@
 class DeceasedsController < ApplicationController
    def new
+      
+   end
+
+   def create
+       @user = User.find(params[:user_id])
+       @deceased = @user.deceaseds.create(deceased_params)
+       redirect_to user_path(@user)
+   end
+
+   private
+   def deceased_params
+      params.require(:deceased).permit(:first_name, :last_name)
    end
 end
