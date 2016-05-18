9559ebdeb33690bbde89014cd4af5cece103a736
Kevin Storberg <kevin@storberg.net>
2016-05-18 18:39:18 -0400

    partially associated Posts with Users

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
    belongs_to :postable, polymorphic: true
-   belongs_to :memorial 
+   belongs_to :memorial
+   belongs_to :user
    # belongs_to :text
    # accepts_nested_attributes_for :postable
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512022702) do
+ActiveRecord::Schema.define(version: 20160518221445) do
 
   create_table "memorials", force: :cascade do |t|
     t.string  "first_name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160512022702) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.integer  "memorial_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["postable_id"], name: "index_posts_on_postable_id"
