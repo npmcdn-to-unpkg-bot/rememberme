d8468b2fff2b4f3188222f5fede2a977816c1e0f
Kevin Storberg <kevin@storberg.net>
2016-05-19 06:43:29 -0400

    minor aesthetic changes to memorials show page

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'bootstrap-sass', '~> 3.3.6'
+# gem 'bootstrap-sass', '~> 4.0.0'
 gem 'autoprefixer-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
    belongs_to :postable, polymorphic: true
    belongs_to :memorial
    belongs_to :user
+   validates :user_id, presence: true 
    # belongs_to :text
    # accepts_nested_attributes_for :postable
 end
