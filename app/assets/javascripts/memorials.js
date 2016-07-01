f93a6132c197ff44bf3e4c6d6a9d30d425de8acd
Kevin Storberg <kevin@Kevins-MacBook-Pro.local>
2016-07-01 17:35:16 -0400

    completed responsive design for navbar in memorials/show

--- app/assets/javascripts/memorials.js
+++ app/assets/javascripts/memorials.js
@@ -2,8 +2,14 @@ var main = function() {
 
 // navbar jQuery =============================================================
    window.onscroll = function() {
+  if ($(this).scrollTop() > 0) {
       $(".navbar-memorial").css({"opacity": "1", "position": "fixed"});
-   };
+
+    } else {
+      $(".navbar-memorial").css({"opacity": "0", "position": "fixed"});
+
+    };
+  };
 
 
 // Modal jQuery ===============================================================
