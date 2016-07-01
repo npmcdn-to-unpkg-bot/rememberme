04f79ea48b94209bcb720e2a5268db5dfe4b6af9
Kevin Storberg <kevin@Kevins-MacBook-Pro.local>
2016-07-01 18:20:57 -0400

    minor styling in memorials/index

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb do
-    process resize_to_fill: [50, 50]
+    process resize_to_fill: [100, 100]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
