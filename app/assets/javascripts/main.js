5e3177c211e6cfa6edd996b8ea2f263da21139ba
Kevin Storberg <kevin@Kevins-MacBook-Pro.local>
2016-07-03 02:03:22 -0400

    nested edit memorials path within navbar with user validation

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,9 @@
+// var main = function() {
+//
+//   $('.btn-success').click(function() {
+//     $(this).css({"background-color": "silver", "border": "0"});
+//   });
+//
+// }; // Main
+//
+// $(document).ready(main);
