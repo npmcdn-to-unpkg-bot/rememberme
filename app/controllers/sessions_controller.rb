e98f00b8f6bb28d1f90e42eae36fd3692ceca89b
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-08-04 00:03:19 -0400

    compiled assets

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsController < ApplicationController
+cclass SessionsController < ApplicationController
   def new
   end
 
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
      session[:user_id] = nil
-     redirect_to root_url, notice: 'Logged Ou!'
+     redirect_to root_url, notice: 'Logged Out!'
   end
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,4 @@
 class Picture < ActiveRecord::Base
    has_many :posts, as: :postable, dependent: :destroy
-   mount_uploader :image, ImageUploader 
+   mount_uploader :image, ImageUploader
 end
