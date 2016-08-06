9a7e4aa5ea63b9a97f11d989cd34582b4ce7151e
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-08-05 20:39:28 -0400

    configuring sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => 'SENDGRID_USERNAME',
   :password => 'SENDGRID_PASSWORD',
-  :domain => 'belltollsfor.me',
+  :domain => 'remember-me-production.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
