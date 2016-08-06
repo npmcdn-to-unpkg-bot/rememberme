8ff6560d8f6201fa93278dc24df535eef13c9dc5
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-08-06 00:16:36 -0400

    downcase email at login

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,9 @@ class User < ActiveRecord::Base
  has_secure_password
 
   before_save :format_user, on: [ :create, :update ]
-  validates :first_name,
-            presence: true
-  validates :last_name,
-            presence: true
-  validates :email,
-            presence: true,
-            uniqueness: true,
-            format: {
-               with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-            }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
    def full_name
       "#{first_name} #{last_name}"
