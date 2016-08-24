8448cc034d8501389a2d802c8284c0fee1364b4c
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-08-24 15:27:13 -0400

    restyled post forms buttons on memorials/show

--- app/assets/javascripts/memorials.js
+++ app/assets/javascripts/memorials.js
@@ -13,27 +13,6 @@ var main = function() {
 
 
 // Post Modals ===============================================================
-   modalRefresh = function() {
-      $('.pictures-form').css("display", "none");
-      $('.texts-form').css("display", "none");
-      $('.videos-form').css("display", "none");
-   };
-
-   $(document).on("click", '.pictures-button', function() {
-      modalRefresh();
-      $('.pictures-form').css("display", "block");
-   });
-
-   $(document).on("click", '.texts-button', function() {
-      modalRefresh();
-      $('.texts-form').css("display", "block");
-   });
-
-   $(document).on("click", '.videos-button', function() {
-      modalRefresh();
-      $('.videos-form').css("display", "block");
-   });
-
    $(document).on("hover", ".pictures-button",
    function() {
      $(this).css("font-size", "20px");
