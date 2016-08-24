1b7b37b8be4ed47f28fff3553c2c43d29f875f77
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-08-24 19:18:06 -0400

    created helper methods for editable rights

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def edit_memorial(current_user, memorial)
+    if memorial != nil && memorial.id != nil && (memorial.user == current_user || current_user.role == 'admin')
+      return true
+    else
+      return false
+    end
+  end
+
 end
--- app/helpers/memorials_helper.rb
+++ app/helpers/memorials_helper.rb
@@ -1,2 +1,11 @@
 module MemorialsHelper
+
+  def edit_post(current_user, memorial, post)
+    if current_user != nil && (memorial.user == current_user || post.user == current_user || current_user.role == 'admin')
+      return true
+    else
+      return false
+    end
+  end
+
 end
